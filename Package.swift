e858f781aafd17c378425071b30f166a40545bc0
Amir <amir.ardalanuk@gmail.com>
2022-10-29 15:18:59 +0330

    init

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WebEngageFramework",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "WebEngageFramework",
+            targets: ["WebengageSPM"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "WebengageSPM",
+            dependencies: [.target(name: "WebEngage", condition: .when(platforms: [.iOS]))]
+        ),
+        .binaryTarget(
+            name: "WebEngage",
+            url:"https://github.com/amir-ardalanuk/WebEngageSPM/raw/master/WebEngage.xcframework.zip",
+            checksum: "4ce62365b6146c1269f1abbcc2fc861ed793335f655f4eadd7cbd22bf283abc4"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# WebengageSPM
+
+A description of this package.
